89dae1218dd85108ca9909bbef1a06a70b89e04a
Digital <Digital@192.168.0.127>
2018-02-13 09:35:34 +0530

    spring boot mvc sample app

---
+++ demo/src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+/**
+ * @author Digital
+ *
+ */
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+	
+	@RequestMapping("/")
+	public String welcome() {
+		return "index";
+	}
+}
